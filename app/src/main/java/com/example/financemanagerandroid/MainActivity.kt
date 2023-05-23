47d2b0fb3226e4c68802fa7dc38a6898fb5a12f4
May <aira.1996@yandex.ru>
2023-05-23 20:17:43 +0300

    - experiments wit listView

--- app/src/main/java/com/example/financemanagerandroid/MainActivity.kt
+++ app/src/main/java/com/example/financemanagerandroid/MainActivity.kt
@@ -1,23 +1,25 @@
 package com.example.financemanagerandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ListView
+import android.widget.SimpleAdapter
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.lang.StringBuilder
 import java.net.HttpURLConnection
 import java.net.URL
-import javax.net.ssl.HttpsURLConnection
 
 class MainActivity() : AppCompatActivity() {
     private lateinit var helloText: TextView
     private lateinit var startButton: ImageButton
+    private lateinit var listDB: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +27,23 @@ class MainActivity() : AppCompatActivity() {
 
         helloText = findViewById(R.id.helloText)
         startButton = findViewById(R.id.startButton)
+        listDB = findViewById(R.id.listDB)
+    }
+
+    fun onGetExpensesButtonClick(view: View) {
+        val arrayOfIds = arrayOf("1", "2")
+        val listOfPersons = mutableListOf<Map<String, String>>()
+        val person1 = parseJson()
+        val person2 = parseJson1()
+
+        listOfPersons.add(0, person1)
+        listOfPersons.add(1, person2)
+
+
+        val adapter = SimpleAdapter(this, listOfPersons, R.layout.listview_item, arrayOf("amount", "comment"),
+            intArrayOf(R.id.textPerson, R.id.textAchievement)
+        )
+        listDB.adapter = adapter
     }
 
     fun onStartButtonClick(view: View) {
@@ -41,6 +60,26 @@ class MainActivity() : AppCompatActivity() {
                     Log.e("MayApp", "There was an IO error", ex)
                 }
             }).start()
+    }
+
+    private fun parseJson(): Map<String, String> {
+        val json =
+            "{\"amount\": 11.1,\"comment\": \"insert through Postman\",\"categoryId\": 1,\"date\": \"2023-05-21T18:15:21.53769\" }"
+        val jsonObject = JSONObject(json)
+        val map = mutableMapOf<String, String>()
+        map["amount"] = jsonObject.getInt("amount").toString()
+        map["comment"] = jsonObject.getString("comment")
+        return map
+    }
+
+    private fun parseJson1(): Map<String, String> {
+        val json =
+            "{\"amount\": 12.0,\"comment\": \"testComment\",\"categoryId\": 1,\"date\": \"2023-05-21T18:15:21.53769\" }"
+        val jsonObject = JSONObject(json)
+        val map = mutableMapOf<String, String>()
+        map["amount"] = jsonObject.getInt("amount").toString()
+        map["comment"] = jsonObject.getString("comment")
+        return map
     }
 
     @Throws(IOException::class)
